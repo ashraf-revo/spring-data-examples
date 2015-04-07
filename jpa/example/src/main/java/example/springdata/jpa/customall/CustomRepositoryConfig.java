e0f5f57a81a4ea91659f84a7e9b0558e7150c5af
Oliver Gierke <ogierke@pivotal.io>
2015-04-07 18:54:06 +0200

    #73 - Added example how to customize all Spring Data repositories.

---
+++ jpa/example/src/main/java/example/springdata/jpa/customall/CustomRepositoryConfig.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package example.springdata.jpa.customall;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+/**
+ * Sample configuration to bootstrap Spring Data JPA through JavaConfig.
+ * 
+ * @author Oliver Gierke
+ * @soundtrack Tim Neuhaus - As life found you (The Cabinet)
+ */
+@Configuration
+@EnableAutoConfiguration
+@EnableJpaRepositories(repositoryBaseClass = ExtendedJpaRepository.class)
+class CustomRepositoryConfig {}
